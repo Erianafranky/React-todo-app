44d34e78cae8a264e1ee4286f8ceb77dcbd6673d
Dorothy Ariana <arianadottie@gmail.com>
2021-06-15 23:14:08 +0300

    Fix add button

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo.jsx';
-import { useParams } from 'react-router';
+
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
-    const params = useParams();
-
-    useEffect(() => {
-		fetch(`https://jsonplaceholder.typicode.com/todos?userId=${params.userid}`)
-			.then((response) => response.json())
-			.then((result) => {
-				setTodos(result);
-			});
-	}, [params]);
-
 
     const addTodo = todo => {
+        
         if (!todo.text || /^\s*$/.test(todo.text)) {
             return;
         }
         const newTodos = [todo, ...todos];
-
         setTodos(newTodos);
+       
         
     };
 
@@ -32,12 +23,14 @@ function TodoList() {
         }
         setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item))
         );
+        
     };
 
     const removeTodo = id => {
         const removeArr = [...todos].filter(todo => todo.id !== id);
 
         setTodos(removeArr);
+       
     };
 
     const completeTodo = id => {
@@ -48,24 +41,20 @@ function TodoList() {
             return todo;
         });
         setTodos(updatedTodos);
+       
     }
 
     return (
         <div>
             <h1>What is the plan for today</h1>
-            {/* <TodoForm onSubmit={addTodo} />
+            <TodoForm onSubmit={addTodo} />
             <Todo todos={todos} completeTodo={completeTodo} 
             removeTodo={removeTodo} updateTodo={updateTodo} />
-             */}
-             <TodoForm onSubmit={addTodo} />
-            {todos.map(() => (
-				<Todo todos = {todos} completeTodo={completeTodo}
-				removeTodo={removeTodo} updateTodo={updateTodo} />
-			))}
-			
             
         </div>
     )
 }
 
 export default TodoList;
+ 
+
